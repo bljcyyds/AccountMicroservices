d6d9134a6ee062c0fe178ee9b114cd84d057d39b
bjxl <1186506721@qq.com>
2023-04-19 15:48:06 +0800

    Initial project files

---
+++ src/test/java/com/cpt304/microservices/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpt304.microservices.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
